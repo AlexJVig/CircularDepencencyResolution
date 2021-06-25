22f0682a44907fb52b0b5a0fe9a7f0ac85f5f1f3
Alex <alzombii@gmail.com>
2021-06-25 16:34:37 +0300

    Started creating the interface solution.

---
+++ CircularDependencyResolutionUsingInterface/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InterfaceSolution
+{
+    interface IConsumer
+    {
+        public void CallFriends();
+    }
+
+    class A
+    {
+        public void MakeCoffee(IConsumer me)
+        {
+            me.CallFriends();
+        }
+    }
+
+    class B : IConsumer
+    {
+        public void CallFriends()
+        {
+            Console.WriteLine("Friends called.");
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
